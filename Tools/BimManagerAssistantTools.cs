be7672d4aec5dd1879709b65cb7651e5de806017
Pedro Nadal <pedronadal02@gmail.com>
2026-07-17 12:18:51 +0200

    create assistant tools

---
+++ Tools/BimManagerAssistantTools.cs
@@ -0,0 +1,15 @@
+﻿using ApsSamples.Services;
+using Autodesk.Construction.AccountAdmin;
+using Autodesk.DataManagement;
+
+namespace ApsSamples.Tools
+{
+    internal class BimManagerAssistantTools(
+        DataManagementClient dataManagementClient,
+        AdminClient adminClient,
+        IUserSessionService session)
+    {
+
+       
+    }
+}
